39115cb5956efafd570c8a4d66bd736dd3a253ae
LEE YEJIN <dldulwls@naver.com>
2024-03-13 13:52:06 +0900

    💄추가페이지 기본구조 세팅 (AddNewsTheme)

---
+++ src/Pages/AddNews.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddNewsForm from "../Components/News/AddnewsForm";
+
+function AddNews() {
+  return (
+    <div>
+      <AddNewsForm />
+    </div>
+  );
+}
+
+export default AddNews;
