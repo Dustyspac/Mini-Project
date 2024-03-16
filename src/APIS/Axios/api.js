54d410f6851a0604fbeaf65b10acb7062c19b7c4
LEE YEJIN <dldulwls@naver.com>
2024-03-16 20:47:07 +0900

    💚env 환경변수 설정

--- src/APIS/Axios/api.js
+++ src/APIS/Axios/api.js
@@ -1,35 +1,34 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: 'https://baekwon.site'
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
 });
 
 request.interceptors.request.use(
-  
-  function(config){
-    console.log(config)
-    config.headers.Authorization =`${window.sessionStorage.getItem('ACCESS_TOKEN')}`;
-    return config
+  function (config) {
+    console.log(config);
+    config.headers.Authorization = `${window.sessionStorage.getItem(
+      "ACCESS_TOKEN"
+    )}`;
+    return config;
   },
 
-  function(error){
+  function (error) {
     console.log(error);
     return Promise.request(error);
   }
-
-)
+);
 
 request.interceptors.response.use(
-  function(response){
-    console.log(response)
-    return response
+  function (response) {
+    console.log(response);
+    return response;
   },
-  
-  function(error){
+
+  function (error) {
     console.log(error);
     return Promise.reject(error);
   }
-
 );
 
 export default request;
